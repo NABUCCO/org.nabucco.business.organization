c846ffaa78d557427899eea767f5a7b905e1d87e
Leonid Agranovskiy <leonid.agranovskiy@prodyna.com>
2012-09-04 11:25:55 +0200

    Release 01.05.00

--- org.nabucco.business.organization.facade.datatype/src/main/gen/org/nabucco/business/organization/facade/datatype/OrganizationMaster.java
+++ org.nabucco.business.organization.facade.datatype/src/main/gen/org/nabucco/business/organization/facade/datatype/OrganizationMaster.java
@@ -1,18 +1,16 @@
 /*
  * Copyright 2012 PRODYNA AG
- *
- * Licensed under the Eclipse Public License (EPL), Version 1.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * 
+ * Licensed under the Eclipse Public License (EPL), Version 1.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the License at
+ * 
  * http://www.opensource.org/licenses/eclipse-1.0.php or
  * http://www.nabucco.org/License.html
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
  */
 package org.nabucco.business.organization.facade.datatype;
 
@@ -27,6 +25,7 @@ import org.nabucco.framework.base.facade.datatype.property.NabuccoProperty;
 import org.nabucco.framework.base.facade.datatype.property.NabuccoPropertyContainer;
 import org.nabucco.framework.base.facade.datatype.property.NabuccoPropertyDescriptor;
 import org.nabucco.framework.base.facade.datatype.property.PropertyCache;
+import org.nabucco.framework.base.facade.datatype.property.PropertyDescriptorSupport;
 
 /**
  * OrganizationMaster
@@ -37,6 +36,8 @@ import org.nabucco.framework.base.facade.datatype.property.PropertyCache;
 public class OrganizationMaster extends Organization implements Datatype {
 
     private static final long serialVersionUID = 1L;
+
+    private static final String[] PROPERTY_CONSTRAINTS = {};
 
     private Long functionalTypeRefId;
 
